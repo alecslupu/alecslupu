cfcc01978c206a2421f84360afced8c208263eb5
Alexandru Emil Lupu <contact@alecslupu.ro>
2024-11-22 17:37:07 +0200

    Add rest of the structure

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,8 @@
 // This is where it all goes :)
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    const text = document.querySelectorAll('.reversed-text');
+    text.forEach((element) => {
+        element.textContent = element.textContent.split('').reverse().join('');
+    })
+});
